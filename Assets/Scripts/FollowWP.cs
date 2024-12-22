80140db55887f05bbc7fd7e33fcbbb0050ab7b80
pabloav57 <pabloav57@gmail.com>
2024-12-22 18:07:41 +0100

    S0503Solution

--- Assets/Scripts/FollowWP.cs
+++ Assets/Scripts/FollowWP.cs
@@ -1,38 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class FollowWP : MonoBehaviour
-{
+public class FollowWP : MonoBehaviour {
+
     public GameObject[] waypoints;
     int currentWP = 0;
 
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+
+    void Start() {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
-        currentWP++;
+    void Update() {
 
-        if(currentWP >= waypoints.Length)
+        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3.0f) {
+
+            currentWP++;
+        }
+
+        if (currentWP >= waypoints.Length) {
+
             currentWP = 0;
+        }
 
-            // this.transform.LookAt(waypoints[currentWP].transform);
+        // this.transform.LookAt(waypoints[currentWP].transform);
 
-            Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
-
-            this.transform.rotation = Quaternion.Slerp(transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
-
-            this.transform.Translate(0, 0,speed * Time.deltaTime);
+        Quaternion lookAtWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, lookAtWP, Time.deltaTime * rotSpeed);
+        this.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
     }
 }
